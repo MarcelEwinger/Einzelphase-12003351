d949098298e34bfec49ff4c76f28b7c480dadedf
MarcelEwinger <marcel.ewinger@alumni.htl-klu.at>
2022-03-12 18:54:06 +0100

    Added TabLayout

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.einzelbeispiel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdpater = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdpater.addFragment(new fragment1(), "Socket");
+        vpAdpater.addFragment(new fragment1(), "Berechnung");
+        viewPager.setAdapter(vpAdpater);
+
     }
 }
